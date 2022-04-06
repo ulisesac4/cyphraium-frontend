ca91ca7a0e8f37888d565197e1a6995f016cc038
ulises avila camargo <ulisesavila04@gmail.com>
2022-04-06 17:04:06 -0500

    delete unnecesary files and unnecesary imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import SubscribeForm from '../components/SubscribeForm';
 import Layout from '../components/Layout';
 
-import pic1 from '../assets/images/01.jpg';
-import pic2 from '../assets/images/02.jpg';
-import pic3 from '../assets/images/03.jpg';
-import pic4 from '../assets/images/04.jpg';
-import pic5 from '../assets/images/05.jpg';
-import pic6 from '../assets/images/06.jpg';
-
 import { Link } from 'gatsby';
 
 const IndexPage = () => (
